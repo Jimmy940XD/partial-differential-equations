0fde6c41f8c1fbf4f3cbaca9d0053a5b8503eff8
Jimmy940XD <jaumesalvadoolius@gmail.com>
2026-03-24 16:32:21 +0000

    Initialize class and create update() method

---
+++ classes.py
@@ -0,0 +1,12 @@
+import numpy as np
+from functions import cahn_hilliard_cfd
+
+class Lattice:
+    def __init__(self, side_len: int, phi0: float):
+        # set-up of the initial lattice
+        base = np.zeros(shape=(side_len, side_len)).fill(phi0)
+        noise = np.random.uniform(-.01, .01, size=(side_len, side_len))
+        self.lattice = base + noise
+
+    def update(self):
+        cahn_hilliard_cfd(self.lattice)
