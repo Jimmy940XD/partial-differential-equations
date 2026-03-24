f411f248736fcc70e6055804245680ed046861bb
Jimmy940XD <jaumesalvadoolius@gmail.com>
2026-03-24 19:27:07 +0000

    Base animation logic

---
+++ animation.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.colors import ListedColormap
+from classes import Lattice
+
+def update(frame):
+    state.evolve()
+
+    img.set_data(state.lattice)
+    return [img]
+
+SIDE_LEN = 50
+PHI0 = float(input("Type an initial value for phi_0: "))
+
+state = Lattice(SIDE_LEN, PHI0)
+
+fig, ax = plt.subplots()
+cmap = ListedColormap(["yellow", "blue"])
+img = ax.imshow(state.lattice, cmap=cmap, interpolation="none")
+
+animation = FuncAnimation(fig, update, frames=None, interval=50, blit=True, cache_frame_data=False)
+plt.show()
