bbe923ab027b3cc2e1a318952a21b46f5961b79c
Jimmy940XD <jaumesalvadoolius@gmail.com>
2026-03-25 21:17:23 +0000

    Change slightly the docstring

--- boundary-value/classes.py
+++ boundary-value/classes.py
@@ -10,8 +10,8 @@ class Lattice:
     def evolve(self):
         """
         Handler method. If assigned to a variable, it updates the
-        lattice to the next iteration and returns difference between
-        the previous and the current lattice. If not assigned to a
-        variable, it only updates the lattice.
+        lattice to the next iteration and returns absolute
+        difference between the previous and the current lattice.
+        If not assigned to a variable, it only updates the lattice.
         """
         return jacobi(self.lattice)
