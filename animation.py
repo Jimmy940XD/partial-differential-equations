001f0aa76f5b0c426a7769ee000d6ee44394e0ff
Jimmy940XD <jaumesalvadoolius@gmail.com>
2026-03-25 00:11:59 +0000

    Finish animation logic

--- animation.py
+++ animation.py
@@ -1,22 +1,23 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-from matplotlib.colors import ListedColormap
+from matplotlib.colors import LinearSegmentedColormap
 from classes import Lattice
 
 def update(frame):
-    state.evolve()
+    for _ in range(100):
+        state.evolve()
 
     img.set_data(state.lattice)
     return [img]
 
-SIDE_LEN = 50
+SIDE_LEN = 100
 PHI0 = float(input("Type an initial value for phi_0: "))
 
 state = Lattice(SIDE_LEN, PHI0)
 
 fig, ax = plt.subplots()
-cmap = ListedColormap(["yellow", "blue"])
-img = ax.imshow(state.lattice, cmap=cmap, interpolation="none", vmin=(PHI0 - .01), vmax=(PHI0 + .01))
+cmap = LinearSegmentedColormap.from_list("yellow_blue", ["yellow", "blue"])
+img = ax.imshow(state.lattice, cmap=cmap, interpolation="bilinear", vmin=-1, vmax=1)
 
-animation = FuncAnimation(fig, update, frames=None, interval=50, blit=True, cache_frame_data=False)
+animation = FuncAnimation(fig, update, frames=None, interval=1, blit=True, cache_frame_data=False)
 plt.show()
