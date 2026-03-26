acce9ecd6c394dd7eced4c9695fc00fac7cd5dc1
Jimmy940XD <jaumesalvadoolius@gmail.com>
2026-03-26 11:32:00 +0000

    Finish convergence.py

--- boundary-value/convergence.py
+++ boundary-value/convergence.py
@@ -3,6 +3,7 @@ from classes import Lattice
 SIDE_LEN = 100
 TOLERANCE = 1e-5
 
+# this loop initializes and runs the system for each method
 METHODS = ("Jacobi", "Gauss-Seidel")
 for method in METHODS:
     state = Lattice(SIDE_LEN)
