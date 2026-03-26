96964fbe246562881c01fdfd609510dd85b49b4c
Jimmy940XD <jaumesalvadoolius@gmail.com>
2026-03-26 21:50:53 +0000

    Reorganize file

--- boundary-value/functions.py
+++ boundary-value/functions.py
@@ -28,28 +28,6 @@ def jacobi(info: tuple):
 
 
 @njit
-def calc_Efield(phi: np.ndarray):
-    """
-    Calculates and returns the 3D electric field at each cell of
-    the lattice based on the current step's potential.
-    """
-    SIDE_LEN = phi.shape[0]
-    E_field = np.zeros(shape=(SIDE_LEN, SIDE_LEN, SIDE_LEN, 3)) # the 3 is for the three-dimensional vector in each cell
-    for i in range(1, SIDE_LEN - 1):
-        for j in range(1, SIDE_LEN - 1):
-            for k in range(1, SIDE_LEN - 1):
-                # calculation of components of electric field using a centred difference
-                Ex = phi[i + 1, j, k] - phi[i - 1, j, k]
-                Ey = phi[i, j + 1, k] - phi[i, j - 1, k]
-                Ez = phi[i, j, k + 1] - phi[i, j, k - 1]
-                # the actual vector per cell
-                E_field[i, j, k, 0] = -1 / 2 * Ex
-                E_field[i, j, k, 1] = -1 / 2 * Ey
-                E_field[i, j, k, 2] = -1 / 2 * Ez
-    return E_field
-
-
-@njit
 def gauss_seidel(info: tuple):
     """
     Updates the current lattice in-place using the red-black Gauss-Seidel method.
@@ -86,6 +64,32 @@ def gauss_seidel(info: tuple):
 
 
 @njit
+def calc_Efield(phi: np.ndarray):
+    """
+    Calculates and returns the 3D electric field at each cell of
+    the lattice based on the current step's potential.
+    """
+    SIDE_LEN = phi.shape[0]
+    E_field = np.zeros(shape=(SIDE_LEN, SIDE_LEN, SIDE_LEN, 3)) # the 3 is for the three-dimensional vector in each cell
+    for i in range(1, SIDE_LEN - 1):
+        for j in range(1, SIDE_LEN - 1):
+            for k in range(1, SIDE_LEN - 1):
+                # calculation of components of electric field using a centred difference
+                Ex = phi[i + 1, j, k] - phi[i - 1, j, k]
+                Ey = phi[i, j + 1, k] - phi[i, j - 1, k]
+                Ez = phi[i, j, k + 1] - phi[i, j, k - 1]
+                # the actual vector per cell
+                E_field[i, j, k, 0] = -1 / 2 * Ex
+                E_field[i, j, k, 1] = -1 / 2 * Ey
+                E_field[i, j, k, 2] = -1 / 2 * Ez
+    return E_field
+
+
+@njit
 def calc_Bfield(Az: np.ndarray):
     SIDE_LEN = Az.shape[0]
-    B_field = np.zeros(shape=(SIDE_LEN, SIDE_LEN, SIDE_LEN, 3))
+    B_field = np.zeros(shape=(SIDE_LEN, SIDE_LEN, SIDE_LEN, 2))
+    for i in range(1, SIDE_LEN - 1):
+        for j in range(1, SIDE_LEN - 1):
+            for k in range(1, SIDE_LEN - 1):
+                Bx = 
