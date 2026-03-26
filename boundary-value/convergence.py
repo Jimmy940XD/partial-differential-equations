14a9cce6b49ebb92daeda5974b7d47ecb0c38a39
Jimmy940XD <jaumesalvadoolius@gmail.com>
2026-03-26 00:52:16 +0000

    Fix bugs

--- boundary-value/convergence.py
+++ boundary-value/convergence.py
@@ -3,13 +3,14 @@ from classes import Lattice
 SIDE_LEN = 100
 TOLERANCE = 1e-5
 
-state = Lattice(SIDE_LEN)
-
 METHODS = ("Jacobi", "Gauss-Seidel")
 for method in METHODS:
+    state = Lattice(SIDE_LEN)
+    n = 0
     convergence = False
     while not convergence:
         distance = state.evolve(method) # read method's docstring for deeper understanding
+        n += 1
         if distance <= TOLERANCE:
             convergence = True
-    print(f"Convergence reached using {method} at distance: {distance}")
+    print(f"{method} converged after {n} iterations at distance: {distance}")
