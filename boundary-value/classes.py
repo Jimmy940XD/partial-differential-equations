a09512451d48cb0d8d812b09f44f1cf0ba59fcf8
Jimmy940XD <jaumesalvadoolius@gmail.com>
2026-03-26 11:56:45 +0000

    Initialize rho

--- boundary-value/classes.py
+++ boundary-value/classes.py
@@ -2,8 +2,20 @@ import numpy as np
 import functions as f
 
 class Lattice:
-    def __init__(self, side_len: int):
+    """
+    General 3D lattice.
+    
+    Attributes
+    ----------
+    self.lattice: numpy.ndarray
+        3D array representing a lattice.
+    self.E_field: numpy.ndarray
+        Property method. Returns an array of 3D electric field vectors
+        at each cell of the lattice.
+    """
+    def __init__(self, side_len: int, rho: np.ndarray):
         self.lattice = np.zeros(shape=(side_len, side_len, side_len))
+        self.rho = rho
 
     def evolve(self, method: str):
         """
@@ -13,11 +25,12 @@ class Lattice:
         current lattice. If not assigned to a variable, it only updates
         the lattice.
         """
+        info = (self.lattice, self.rho)
         method = method.lower()
         if method == "jacobi":
-            return f.jacobi(self.lattice)
+            return f.jacobi(info)
         if method == "gauss-seidel":
-            return f.gauss_seidel(self.lattice)
+            return f.gauss_seidel(info)
     
     @property
     def E_field(self):
