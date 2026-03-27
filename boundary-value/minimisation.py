0e08f0167f858ab3f9cb550aa2ae88a494d676b0
Jimmy940XD <jaumesalvadoolius@gmail.com>
2026-03-27 00:44:54 +0000

    Fix bugs and add plot

--- boundary-value/minimisation.py
+++ boundary-value/minimisation.py
@@ -1,5 +1,6 @@
 from classes import Lattice
 import numpy as np
+import matplotlib.pyplot as plt
 
 SIDE_LEN = int(input("Choose the side length of the system: "))
 TOLERANCE = float(input("Choose the accuracy of the solution: "))
@@ -10,11 +11,10 @@ current = np.zeros(shape=(SIDE_LEN, SIDE_LEN, SIDE_LEN))
 MID = SIDE_LEN // 2
 current[MID, MID, 1:-1] = 1
 
-state = Lattice(SIDE_LEN, current)
-
 iterations = []
-omegas = np.arange(1, 2 + STEP, STEP) # initialize omega values
+omegas = np.arange(1, 2, STEP) # initialize omega values
 for omega in omegas:
+    state = Lattice(SIDE_LEN, current)
     convergence = False
     n = 0
     while not convergence:
@@ -26,3 +26,11 @@ for omega in omegas:
 index_min = np.argmin(iterations) # get the index for the minimum value
 omega_min = omegas[index_min]
 print(f"With 1 <= ω <= 2 and {STEP} separation, ω = {omega_min} shortens convergence time the most.")
+
+plt.figure()
+plt.plot(omegas, iterations)
+plt.xlabel(r"$\omega$")
+plt.ylabel(r"# iteration $n$")
+plt.title(r"# iterations per each value of $\omega$")
+plt.savefig(f"./plots/omega.png", dpi=300)
+plt.show()
