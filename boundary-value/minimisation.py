d576503b42b20080b09c0e4badf9430fad75e5a4
Jimmy940XD <jaumesalvadoolius@gmail.com>
2026-03-27 00:16:39 +0000

    Finish minimisation logic

---
+++ boundary-value/minimisation.py
@@ -0,0 +1,28 @@
+from classes import Lattice
+import numpy as np
+
+SIDE_LEN = int(input("Choose the side length of the system: "))
+TOLERANCE = float(input("Choose the accuracy of the solution: "))
+STEP = .01
+# current is chosen because it takes longer than point charge to converge with Gauss-Seidel
+# main goal is to see clear faster convergence but edit as desired
+current = np.zeros(shape=(SIDE_LEN, SIDE_LEN, SIDE_LEN))
+MID = SIDE_LEN // 2
+current[MID, MID, 1:-1] = 1
+
+state = Lattice(SIDE_LEN, current)
+
+iterations = []
+omegas = np.arange(1, 2 + STEP, STEP)
+for omega in omegas:
+    convergence = False
+    n = 0
+    while not convergence:
+        diff = state.evolve("sor", omega)
+        n += 1
+        if diff <= TOLERANCE:
+            convergence = True
+    iterations.append(n)
+index_min = np.argmin(iterations)
+omega_min = omegas[index_min]
+print(f"With 1 <= ω <= 2 and {STEP} separation, ω = {omega_min} shortens convergence time the most.")
