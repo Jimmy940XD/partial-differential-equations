6db3e3c44719e2d0b86eac04d1bc422d3b629132
Jimmy940XD <jaumesalvadoolius@gmail.com>
2026-03-27 02:36:47 +0000

    Change to account for SOR

--- boundary-value/convergence.py
+++ boundary-value/convergence.py
@@ -4,13 +4,17 @@ SIDE_LEN = 100
 TOLERANCE = 1e-5
 
 # this loop initializes and runs the system for each method
-METHODS = ("Jacobi", "Gauss-Seidel")
+METHODS = ("Jacobi", "Gauss-Seidel", "SOR")
 for method in METHODS:
     state = Lattice(SIDE_LEN)
+    if method.lower() == "sor":
+        omega = 1.78 # fastest
+    else:
+        omega = None
     n = 0
     convergence = False
     while not convergence:
-        distance = state.evolve(method) # read method's docstring for deeper understanding
+        distance = state.evolve(method=method, omega=omega) # read method's docstring for deeper understanding
         n += 1
         if distance <= TOLERANCE:
             convergence = True
